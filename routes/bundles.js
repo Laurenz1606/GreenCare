4cb33b43d9426c3b0c4a25d0a8799a27b2a01760
Laurenz1606 <laurenz.rausche@gmail.com>
2021-01-28 17:57:33 +0100

    Header and Footer/Bundle routes

--- routes/bundles.js
+++ routes/bundles.js
@@ -18,16 +18,28 @@ router.get('/:id', (req, res) => {
     if (titles.includes(req.params.id)) {
         var index = titles.findIndex(element => element.includes(req.params.id))
         bundles = require('../Content/bundles.json')
-        res.render('index', Object.assign({}, res.locals, {
-            title: titles[index],
-            bundle: bundles[i]
+        res.render('bundles/show', Object.assign({}, res.locals, {
+            title: bundles[index].title,
+            bundle: bundles[index]
+        }))
+        return
+    }
+    for (var i = 0; i < titles.length; i++) {
+        titles[i] = titles[i].toLowerCase()
+    }   
+    if (titles.includes(req.params.id)) {
+        var index = titles.findIndex(element => element.includes(req.params.id))
+        res.redirect(capitalizeFirstLetter(bundles[index].title))
+    }
+    else {
+        res.status(404).render('404', Object.assign({}, res.locals, {
+            title: 'Seite nicht gefunden'
         }))
     }
-    else (
-        res.status(404).render('404', Object.assign({}, res.locals, {
-        title: 'Seite nicht gefunden'
-    }))
-    )
 })
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 module.exports = router
