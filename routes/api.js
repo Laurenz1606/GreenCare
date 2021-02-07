006e89346d35946619a6332daae8307a6b2a8bc1
Laurenz1606 <laurenz.rausche@gmail.com>
2021-02-07 19:23:18 +0100

    Friend introduction in api

--- routes/api.js
+++ routes/api.js
@@ -6,27 +6,6 @@ const User = require('../models/user')
 const Bundle = require('../models/bundle')
 const mongoose = require('mongoose')
 
-// Get all user params by token
-router.post('/user', async (req, res) => {
-    let error = {}
-    if (req.body.token) {
-        let data
-        data = await User.find({ apiToken: req.body.token })
-        data = data[0]
-        if (data == null || data == undefined) {
-            error.message = 'Prüfe deinen Token'
-            res.status(404).json(error)
-        }
-        else if (req.body.token == data.apiToken){   
-            res.status(201).json(data)
-        }
-    }
-    else {
-        error.message = 'Gebe den apiToken an'
-        res.status(404).json(error)
-    }
-})
-
 //Get userinfo params by token
 router.post('/userinfo', async (req, res) => {
     let error = {}
@@ -43,6 +22,19 @@ router.post('/userinfo', async (req, res) => {
             infos.name = data.name
             infos.email = data.email
             infos.points = data.points
+            infos.friends = []
+            if (data.friends.length > 0) {
+                for(const id of data.friends) {
+                    let user = await User.findById(id)
+                    if(!user.private) {
+                        let userInfo = {}
+                        userInfo.name = user.name
+                        userInfo.email = user.email
+                        userInfo.points = user.points
+                        infos.friends.push(userInfo)
+                    }
+                }
+            }
             infos.bundle = {}
             if (data.activated) {
                 let bundle = await Bundle.findById(data.linked)
